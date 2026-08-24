5a3a544a27e1e52c9ea5b767a4d92b9d4bca5ad1
User <chinitobnlokillo@gmail.com>
2026-08-24 01:14:53 -0400

    Quita archivos de prueba de logos que se colaron sin querer

--- _convert_logos_tmp.js
+++
@@ -1,7 +0,0 @@
-const sharp = require('sharp');
-const DESK = 'C:\\Users\\chini\\OneDrive\\Desktop\\BrandUp-video-referencias\\';
-(async () => {
-  await sharp(DESK + '02-logo-icono-B-flecha.webp').resize(600).png().toFile(DESK + '02-logo-icono-B-flecha.png');
-  await sharp(DESK + '03-logo-completo-wordmark.webp').resize(1200).png().toFile(DESK + '03-logo-completo-wordmark.png');
-  console.log('ok');
-})();
